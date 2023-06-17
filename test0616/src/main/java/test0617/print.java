74f1cb83610734074e0ad9d584533545494c9cb2
YMJ936 <133292703+YMJ936@users.noreply.github.com>
2023-06-17 19:44:07 +0900

    학원 문제 풀이, 코딩 테스트 기초 문제

---
+++ test0616/src/main/java/test0617/print.java
@@ -0,0 +1,14 @@
+package test0617;
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello");
+		System.out.printf("Hello World");
+		System.out.printf("Hello\nWorld");
+		System.out.printf("Hello\tWorld");
+		
+	}
+
+}
